807f2edb6cd7e5d6c45ae1f47ca0ded18b538391
CNR <narayana.cheerla@gmail.com>
2024-07-28 13:14:56 +0530

    removed un used stories

--- packages/components/src/components/Button.tsx
+++ packages/components/src/components/Button.tsx
@@ -3,12 +3,33 @@ import "../styles/input.css";
 
 type ButtonProps = {
   textColor?: string;
-  children: React.ReactNode;
+  title: string;
   disable?: boolean;
+  backgroundColor?: string;
+  padding?: string;
+  fontSize?: string;
+  margin?: string;
+  borderColor?:string;
 };
 
-const Button = ({ textColor = "white", children, disable = false }: ButtonProps) => {
-  return <button className={`bg-blue-400 rounded-lg p-2 text-lg border border-solid border-blue-700 m-1 ${disable ? "bg-gray-400 border-gray-500 pointer-events-none": "bg-blue-500 border-blue-500"}`} style={{ color: textColor }}>{children}</button>;
+const Button = ({
+  textColor = "white",
+  title = "Default",
+  disable = false,
+  backgroundColor = "bg-blue-400",
+  padding = "p-2",
+  fontSize = "text-md",
+  margin = "m-1",
+  borderColor = "border-blue-700",
+}: ButtonProps) => {
+  return (
+    <button
+      className={`${backgroundColor} ${padding} rounded-lg ${padding} ${fontSize} border border-solid ${borderColor} ${margin} ${disable ? "bg-gray-400 border-gray-500 pointer-events-none" : "bg-blue-500 border-blue-500"}`}
+      style={{ color: textColor }}
+    >
+      {title}
+    </button>
+  );
 };
 
 export default Button;
