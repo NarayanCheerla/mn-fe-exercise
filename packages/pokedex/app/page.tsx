90ace69481349e12a853f33852da304959768a3c
CNR <narayana.cheerla@gmail.com>
2024-07-28 14:58:45 +0530

    utils pacakge added

--- packages/pokedex/app/page.tsx
+++ packages/pokedex/app/page.tsx
@@ -1,9 +1,11 @@
 "use client"
 import {Button} from "components";
+import {ConvertAsHeading} from "utils";
 
 export default function Home() {
   return (
     <div>
+      <h1>{ConvertAsHeading("text-weight-super")}</h1>
       <Button onClick={() => alert("default button clicked")}>Click Default Button</Button>
       <Button disable onClick={() => alert("disabled button clicked")}>Disabled</Button>
     </div>
---
+++ packages/utils/README.md
@@ -0,0 +1,11 @@
+# `ts-package-2`
+
+> TODO: description
+
+## Usage
+
+```
+const tsPackage2 = require('ts-package-2');
+
+// TODO: DEMONSTRATE API
+```
